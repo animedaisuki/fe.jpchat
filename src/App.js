62bea9f4b88e886f6ddba24cf7f0abba0fb845e0
Summy <s4594789@student.uq.edu.au>
2023-03-17 11:12:43 +1000

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(1);
+  return <div className="App">123</div>;
 }
 
 export default App;
