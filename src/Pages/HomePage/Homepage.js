633f112dc17b2d1e555b76f7e84ccee8ce427d36
Summy <s4594789@student.uq.edu.au>
2023-03-17 16:45:23 +1000

    login page

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -19,6 +19,23 @@ export default function Homepage() {
               JP Chat is Now Released on Multiple Platforms!
             </h1>
           </div>
+          <div className={styles.platformContainer}>
+            <img
+              className={styles.platformImg}
+              src="https://webstatic.hoyoverse.com/upload/event/2020/12/14/6ff53bc5d797e9efc2ac7b2b602c267a_8295237617862628623.png"
+              alt="apple"
+            />
+            <img
+              className={styles.platformImg}
+              src=" https://webstatic.hoyoverse.com/upload/event/2020/12/14/396b8eb0d0d2f27aadb476e0b62d0926_8878641138830187947.png"
+              alt="google"
+            />
+            <img
+              className={styles.platformImg}
+              src="https://webstatic.hoyoverse.com/upload/event/2020/12/14/a27edf5aa048e9509f0850856ad3f403_396608336029707725.png"
+              alt="windows"
+            />
+          </div>
         </div>
       </div>
       <div className={styles.test}>12345</div>
