2e74c812bf6f1af19f4f3a81627fa9f0526bb1ec
Summy <s4594789@student.uq.edu.au>
2023-03-29 08:37:26 +1000

    refactor friends sidebar to components

--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -2,76 +2,9 @@ import React from "react";
 import styles from "./ChatPage.module.scss";
 import { RiLogoutBoxFill } from "react-icons/ri";
 import { IoSettingsSharp } from "react-icons/io5";
-import { FaUserFriends } from "react-icons/fa";
-import ChatFriendView from "../../components/ChatFriendView/ChatFriendView";
-import { v4 as uuid } from "uuid";
+import ChatFriendsSideBar from "../../components/ChatFriendsSideBar/ChatFriendsSideBar";
 
 export default function ChatPage() {
-  const friendZura = {
-    id: uuid(),
-    username: "ZuraHararara",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-ayaka-small.png",
-  };
-  const friendTsu = {
-    id: uuid(),
-    username: "TsuyokuYoshiko",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-nahida-small.png",
-  };
-  const friendHosoya = {
-    id: uuid(),
-    username: "Hosoya",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-shogun-samll.png",
-  };
-  const friendZiqi = {
-    id: uuid(),
-    username: "Ziqi",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-kujou-small.png",
-  };
-  const friendDavid = {
-    id: uuid(),
-    username: "David",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-shogun-samll.png",
-  };
-  const friendZijun = {
-    id: uuid(),
-    username: "Zijun",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-yoimiya-small.png",
-  };
-  const friendJC = {
-    id: uuid(),
-    username: "JC",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-nahida-small.png",
-  };
-  const friendJason = {
-    id: uuid(),
-    username: "Jason",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-ayaka-small.png",
-  };
-  const friendJoe = {
-    id: uuid(),
-    username: "Joe",
-    avatar:
-      "https://amahane.s3.ap-northeast-1.amazonaws.com/users/defaultAvatar/char-shogun-samll.png",
-  };
-  const friends = [
-    friendZura,
-    friendTsu,
-    friendHosoya,
-    friendZiqi,
-    friendDavid,
-    friendZijun,
-    friendJC,
-    friendJason,
-    friendJoe,
-  ];
   return (
     <div className={styles.chatPageContainer}>
       <div className={styles.chatWindowContainer}>
@@ -96,32 +29,7 @@ export default function ChatPage() {
                 placeholder="Find or start a conversation"
               />
             </div>
-            <div className={styles.chatWindowLeftFunctionBarFriendsContainer}>
-              <div
-                className={styles.chatWindowLeftFunctionBarFriendsBtnContainer}
-              >
-                <div className={styles.chatWindowLeftFunctionBarFriendsBtn}>
-                  <div
-                    className={
-                      styles.chatWindowLeftFunctionBarFriendsBtnIconContainer
-                    }
-                  >
-                    <FaUserFriends size={30} />
-                  </div>
-                  <p className={styles.chatWindowLeftFunctionBarFriendsBtnDesc}>
-                    Friends
-                  </p>
-                </div>
-              </div>
-              {friends.map((friend) => (
-                <ChatFriendView
-                  key={friend.id}
-                  id={friend.id}
-                  username={friend.username}
-                  avatar={friend.avatar}
-                />
-              ))}
-            </div>
+            <ChatFriendsSideBar />
           </div>
         </div>
         <div className={styles.chatWindowRightContent}></div>
