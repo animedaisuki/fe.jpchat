ba7b6fa562d6dbe66ce0cac77ded0cb731d441f0
Summy <s4594789@student.uq.edu.au>
2023-04-01 22:52:15 +1000

    use set message

--- src/components/ChatWindowRightContent/ChatDetails/ChatDetails.js
+++ src/components/ChatWindowRightContent/ChatDetails/ChatDetails.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./ChatDetails.module.scss";
 import { CurrentFriendContext } from "../../../context/CurrentFriendInfoProvider";
@@ -10,46 +10,21 @@ import { v4 as uuid } from "uuid";
 export default function ChatDetails() {
   const { chatId } = useParams();
   const currentFriend = useContext(CurrentFriendContext);
-  const messages = [
-    {
-      id: uuid(),
-      sender: "21f307a2-b840-414e-9f2b-9f070979ba63",
-      receiver: "me",
-      text: "喜欢二次元",
-    },
-    {
-      id: uuid(),
-      sender: "me",
-      receiver: "21f307a2-b840-414e-9f2b-9f070979ba63",
-      text: "我也喜欢二次元",
-    },
-    {
-      id: uuid(),
-      sender: "21f307a2-b840-414e-9f2b-9f070979ba63",
-      receiver: "me",
-      text:
-        "Hello a Hello a Hello a Hello a Hello a Hello a Hello a " +
-        "Hello a Hello a Hello a Hello a Hello a Hello a " +
-        "Hello a Hello a Hello a Hello a Hello a",
-    },
-    {
-      id: uuid(),
-      sender: "21f307a2-b840-414e-9f2b-9f070979ba63",
-      receiver: "me",
-      text: "测试测试",
-    },
-    {
-      id: uuid(),
-      sender: "me",
-      receiver: "21f307a2-b840-414e-9f2b-9f070979ba63",
-      text: "好欸好欸",
-    },
-  ];
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState(null);
+
+  const handleChatInputChange = (e) => {
+    if (e.key !== "Enter") {
+      setInputValue(e.target.value);
+    }
+  };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       //提交内容
       console.log("提交");
+      //清空输入框
+      setInputValue("");
     }
   };
 
@@ -59,24 +34,28 @@ export default function ChatDetails() {
         <div className={styles.chatOverviewContainer}>
           <BsFillChatDotsFill size={20} />
           <p className={styles.chatOverviewUsername}>
-            {currentFriend.username}
+            {currentFriend?.username}
           </p>
         </div>
       </div>
       <div className={styles.chatDetailsInfoAndChatContainer}>
         <FriendInfo />
-        {messages.map((message) => (
+        {messages?.map((message) => (
           <ChatMessage message={message} />
         ))}
       </div>
       <div className={styles.sendMessageInputContainer}>
         <input
           className={styles.sendMessageInput}
+          value={inputValue}
+          onChange={(e) => {
+            handleChatInputChange(e);
+          }}
           onKeyPress={(e) => {
             handleKeyPress(e);
           }}
           type="text"
-          placeholder={`Message @${currentFriend.username}`}
+          placeholder={`Message @${currentFriend?.username}`}
         />
       </div>
     </div>
