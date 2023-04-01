5f2231c04316580c3d6743eec86c35068f914592
Summy <s4594789@student.uq.edu.au>
2023-04-02 08:43:32 +1000

    add send message function in chatDetails component

--- src/components/ChatWindowRightContent/ChatDetails/ChatDetails.js
+++ src/components/ChatWindowRightContent/ChatDetails/ChatDetails.js
@@ -6,10 +6,12 @@ import { BsFillChatDotsFill } from "react-icons/bs";
 import FriendInfo from "./FriendInfo/FriendInfo";
 import ChatMessage from "./ChatMessage/ChatMessage";
 import { v4 as uuid } from "uuid";
-import { getMessage } from "../../../api/message/message";
+import { getMessage, sendMessage } from "../../../api/message/message";
+import { UserContext } from "../../../context/UserInfoProvider";
 
 export default function ChatDetails() {
   const { conversationId } = useParams();
+  const user = useContext(UserContext);
   const currentFriend = useContext(CurrentFriendContext);
   const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState([]);
@@ -28,12 +30,20 @@ export default function ChatDetails() {
     }
   };
 
-  const handleKeyPress = (e) => {
+  const handleKeyPress = async (e) => {
     if (e.key === "Enter") {
-      //提交内容
-      console.log("提交");
-      //清空输入框
-      setInputValue("");
+      //如果输入框不为空
+      if (e.target.value.trim() !== "") {
+        //提交内容
+        const messageData = {
+          senderId: user.id,
+          conversation: conversationId,
+          text: inputValue,
+        };
+        const result = await sendMessage(messageData);
+        //清空输入框
+        setInputValue("");
+      }
     }
   };
 
