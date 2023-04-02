be758979bf9b59cea9c8a34ac4d0ba5adc66ef28
Summy <s4594789@student.uq.edu.au>
2023-04-02 20:12:35 +1000

    use socket.io in express

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,8 @@ const config = {
   apiAddress:
     process.env.REACT_APP_BACKEND_URL || "https://amahanechat.fly.dev/api/v1",
   hCaptchaSiteKey: process.env.REACT_APP_HCAPTCHA_SITE_KEY || "123456",
+  socketServerAddress:
+    process.env.REACT_APP_SOCKET_SERVER_URL || "https//amahanechat-so.fly.dev",
 };
 
 export default config;
