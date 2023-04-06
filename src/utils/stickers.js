83218237faf68e193f15a83918cf8565c2de0c5b
Summy <s4594789@student.uq.edu.au>
2023-04-06 10:22:30 +1000

    stickers utils

---
+++ src/utils/stickers.js
@@ -0,0 +1,68 @@
+import happySticker from "../assets/mafumafuStickers/16873560@2x.png";
+import hiddeSticker from "../assets/mafumafuStickers/16873539@2x.png";
+import stareSticker from "../assets/mafumafuStickers/16873538@2x.png";
+import angrySticker from "../assets/mafumafuStickers/16873541@2x.png";
+import crySticker from "../assets/mafumafuStickers/16873542@2x.png";
+import strongCrySticker from "../assets/mafumafuStickers/16873550@2x.png";
+import questionSticker from "../assets/mafumafuStickers/16873543@2x.png";
+import loveSticker from "../assets/mafumafuStickers/16873545@2x.png";
+import StrongLoveSticker from "../assets/mafumafuStickers/16873544@2x.png";
+import fuSticker from "../assets/mafumafuStickers/16873553@2x.png";
+import lieSticker from "../assets/mafumafuStickers/16873555@2x.png";
+import shySticker from "../assets/mafumafuStickers/16873567@2x.png";
+import sorrySticker from "../assets/mafumafuStickers/16873569@2x.png";
+import dameSticker from "../assets/mafumafuStickers/16873572@2x.png";
+import okSticker from "../assets/mafumafuStickers/16873571@2x.png";
+
+export const stickers = [
+  { unicode: "u{1F600}", png: happySticker },
+  {
+    unicode: "u{1F910}",
+    png: hiddeSticker,
+  },
+  { unicode: "u{1F612}", png: stareSticker },
+  { unicode: "u{1F623}", png: angrySticker },
+  {
+    unicode: "u{1F622}",
+    png: crySticker,
+  },
+  { unicode: "u{1F62D}", png: strongCrySticker },
+  {
+    unicode: "u{1F635}",
+    png: questionSticker,
+  },
+  { unicode: "u{1F970}", png: loveSticker },
+  { unicode: "u{1F60D}", png: StrongLoveSticker },
+  { unicode: "u{2705}", png: dameSticker },
+  { unicode: "u{274E}", png: okSticker },
+  { unicode: "u{1F636}", png: fuSticker },
+  {
+    unicode: "u{1F971}",
+    png: lieSticker,
+  },
+  { unicode: "u{1F648}", png: shySticker },
+  { unicode: "u{1F63F}", png: sorrySticker },
+];
+
+// const emojis = [
+//   ":smile:",
+//   ":thumbsup:",
+//   ":heart:",
+//   ":joy:",
+//   ":sunglasses:",
+//   ":grinning:",
+//   ":wink:",
+//   ":disappointed:",
+//   ":worried:",
+//   ":confused:",
+//   ":+1:",
+//   ":-1:",
+//   ":100:",
+//   ":heavy_check_mark:",
+//   ":x:",
+// ];
+
+// const regex = /\[\d+\]/g;
+// const input = "interesting more [1]commu[88]nities[100] out here![2]";
+// const matches = input.match(regex);
+// console.log(matches);
