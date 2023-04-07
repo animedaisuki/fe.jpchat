d4830884a7cf9a6126410a07304546fab66c1a45
Summy <s4594789@student.uq.edu.au>
2023-04-07 18:10:02 +1000

    add more context

--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -5,19 +5,25 @@ import ChatWindowLeftContent from "../../components/ChatWindowLeftContent/ChatWi
 import { SocketInfoProvider } from "../../context/SocketRefProvider";
 import { ConversationProvider } from "../../context/ConversationProvider";
 import { FriendsOfUserProvider } from "../../context/FriendsOfUserProvider";
+import { ChatGptConversationProvider } from "../../context/ChatGptConversationProvider";
+import { AIFriendsOfUserProvider } from "../../context/AIFriendOfUserProvider";
 
 export default function ChatPage() {
   return (
     <SocketInfoProvider>
       <ConversationProvider>
         <FriendsOfUserProvider>
-          <div className={styles.chatPageContainer}>
-            <div className={styles.chatWindowContainer}>
-              <ChatWindowLeftContent />
-              {/*<div className={styles.chatWindowRightContent}></div>*/}
-              <Outlet />
-            </div>
-          </div>
+          <ChatGptConversationProvider>
+            <AIFriendsOfUserProvider>
+              <div className={styles.chatPageContainer}>
+                <div className={styles.chatWindowContainer}>
+                  <ChatWindowLeftContent />
+                  {/*<div className={styles.chatWindowRightContent}></div>*/}
+                  <Outlet />
+                </div>
+              </div>
+            </AIFriendsOfUserProvider>
+          </ChatGptConversationProvider>
         </FriendsOfUserProvider>
       </ConversationProvider>
     </SocketInfoProvider>
