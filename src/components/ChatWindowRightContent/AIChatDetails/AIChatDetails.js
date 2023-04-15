1130c0fe2d8531bed6696511318747a3452d0c51
Wendy <xccxccxccb57@gmail.com>
2023-04-15 17:23:13 +1000

    sse，暂时不要merge

--- src/components/ChatWindowRightContent/AIChatDetails/AIChatDetails.js
+++ src/components/ChatWindowRightContent/AIChatDetails/AIChatDetails.js
@@ -11,18 +11,116 @@ import { FaTelegramPlane } from "react-icons/fa";
 import { sendMessageToAI } from "../../../api/chatGpt/chatGpt";
 import { fetchAIMessagesByConversationId } from "../../../api/chatGpt/chatGpt";
 import { useSelector } from "react-redux";
+import config from "../../../config/config";
 
 export default function AIChatDetails() {
   const { conversationId } = useParams();
   const user = useContext(UserContext);
   const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState([]);
+
+  //这是用于http请求的，暂时保留
   const [arrivalMessages, setArrivalMessages] = useState(null);
+  //这是用于处理sse请求的
+  const [arrivalMsg, setArrivalMsg] = useState(null);
+  const [AIIsTyping, setAIIsTyping] = useState(false);
+  const [messageAdded, setMessageAdded] = useState(false);
+  const [text, setText] = useState("");
+  const [messageQueen, setMessageQueen] = useState([]);
+
   const [isDisabled, setIsDisabled] = useState(false);
   const scrollRef = useRef();
+  const scrollContainerRef = useRef(null);
+  const eventSourceRef = useRef(null);
 
   const AIFriends = useSelector((state) => state.friendsOfUser.AIFriends);
   const [currentAIFriend, setCurrentAIFriend] = useState(null);
+
+  useEffect(() => {
+    eventSourceRef.current = new EventSource(
+      `${config.apiAddress}/openai/chat`
+    );
+
+    if (typeof eventSource !== "undefined") {
+      console.log("event source on");
+    }
+
+    eventSourceRef.current.onopen = () => {
+      console.log("EventSource connection opened");
+    };
+
+    eventSourceRef.current.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      //防止速度过快导致信息丢失
+      setMessageQueen((prevState) => [...prevState, message]);
+    };
+    // eventSource.onerror = (error) => {
+    //   console.error("EventSource error:", error);
+    // };
+    return () => {
+      if (eventSourceRef.current) {
+        eventSourceRef.current.close();
+      }
+    };
+  }, [conversationId, inputValue]);
+
+  useEffect(() => {
+    if (messageQueen.length > 0) {
+      const message = messageQueen[0];
+      if (message.type === "end") {
+        console.log("All messages received, end of stream.");
+        setTimeout(() => {
+          setText("");
+        }, 2000);
+        if (eventSourceRef.current) {
+          eventSourceRef.current.close();
+        }
+        setTimeout(() => {
+          setAIIsTyping(false);
+        }, 2000);
+        setTimeout(() => {
+          setMessageAdded(false);
+        }, 2000);
+      } else {
+        if (!AIIsTyping) {
+          setAIIsTyping(true);
+        }
+        // console.log("Received message:", message);
+        if (message?.text) {
+          setText((prevState) => prevState + message?.text);
+        }
+      }
+      setMessageQueen((prevState) => prevState.slice(1));
+      // setTimeout(() => {}, 50);
+    }
+  }, [AIIsTyping, messageQueen]);
+
+  useEffect(() => {
+    if (text !== "") {
+      setArrivalMsg({
+        conversationId,
+        text,
+        senderId: currentAIFriend,
+      });
+    }
+  }, [conversationId, currentAIFriend, text]);
+
+  useEffect(() => {
+    if (AIIsTyping && arrivalMsg?.text !== "" && !messageAdded && arrivalMsg) {
+      setMessages([...messages, arrivalMsg]);
+      setMessageAdded(true);
+    }
+  }, [AIIsTyping, arrivalMsg, messageAdded, messages]);
+
+  useEffect(() => {
+    if (messageAdded && text) {
+      const newMessages = [...messages];
+      newMessages[newMessages.length - 1].text = text;
+      setMessages(newMessages);
+      scrollContainerRef.current.scrollTop =
+        scrollContainerRef?.current?.scrollHeight;
+    }
+  }, [messageAdded, messages, text]);
 
   useEffect(() => {
     setCurrentAIFriend(AIFriends[0]?.user);
@@ -94,9 +192,10 @@ export default function AIChatDetails() {
     }
   };
 
-  useEffect(() => {
-    setMessages((prevState) => [...prevState, arrivalMessages]);
-  }, [arrivalMessages]);
+  //这是用于处理http请求的，暂时保留
+  // useEffect(() => {
+  //   setMessages((prevState) => [...prevState, arrivalMessages]);
+  // }, [arrivalMessages]);
 
   return (
     <div className={styles.chatDetailsContainer}>
@@ -108,7 +207,10 @@ export default function AIChatDetails() {
           </p>
         </div>
       </div>
-      <div className={styles.chatDetailsInfoAndChatContainer}>
+      <div
+        className={styles.chatDetailsInfoAndChatContainer}
+        ref={scrollContainerRef}
+      >
         <AIFriendInfo currentAIFriend={currentAIFriend} />
         {messages?.map((message) => (
           <div key={uuid()} ref={scrollRef}>
